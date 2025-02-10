190ebe578eac3c400d6543aaa41ba5d9f42fdd74
Ayden <anewell8331@student.pps.net>
2025-02-10 13:06:09 -0800

    aaa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
     public List<Card> deck = new List<Card>();
-    public List<Card> player_deck = new List<Card>();
-    public List<Card> ai_deck = new List<Card>();
     public List<Card> player_hand = new List<Card>();
     public List<Card> ai_hand = new List<Card>();
     public List<Card> discard_pile = new List<Card>();
+
+    public int targetHandSize = 21;
+    public int playerSpecialCards = 0;
+    public int aiSpecialCards = 0;
+    //These will reset every game
+    public int playerGamesWon = 0;
+    public int aiGamesWon = 0;
+
+
 
     private void Awake()
     {
